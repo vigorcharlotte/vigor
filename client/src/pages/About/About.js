a59083fff857d05c614f1237bf04cbd0736a20e0
kmboyle <kevin.michael.boyle@gmail.com>
2017-12-19 20:41:06 -0500

    changed about us

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -23,7 +23,18 @@ class About extends Component {
             </CardMedia>
             <CardTitle title="About us" />
             <CardText>
-              At Vigor we are revolutionizing the massage therapy and personal training industry by offering a unified solution that no one can rival. The beginning of the journey begins at a question, Where Does It Hurt? In this phase our clients begin by selecting a massage or the patented techniques of movement education we have developed to alleviate their pains. Not a single movement of our client's goes unanalyzed, our professionals are trained to asses patterns in movement in order to accurately solve the problem. With an entirely personalized array of stretches and movements, each client is treated individually with precision and care. As the body recovers our team trains our clients from diagnosing the problem, to solving it, to making sure it isn't privy to resurface. Ultimately to become independent of all therapists, including ourselves, and regain control of their life.
+             <p>What you will find at Vigor is several different platforms that can and will assist you in becoming a healthy and new you.</p>
+             <p>From the inside out, we have a unique infrastructure built with our team and partners, Where Does It Hurt, and Black Sheep Massage.</p>
+             <p>These companies were built to work along side Vigor to broaden our scope of professional tools, so that we can do what others cannot.</p>
+             <p>It's a complete system tailored to fit your bodies every need.  Our customer service is for sure the best; we take great pride in treating
+               our clients, customers, and members like they should be treated.</p>
+             <p>Our complete system's drive and effort is to help make you the best you can be.  If you just want to relax and drink coffee with a Vigor smile,
+               or if you are working with Where Does It Hurt, trying to become independent of all therapists.</p>
+             <p>Also, don't forget SAVINGS.  Our members save more money a year on just coffee alone.  You have to, right??</p>
+             <p>Again, superior customer service is the most important aspect of our company and we are here to earn your trust from the start.</p>
+            
+
+
             </CardText>
             <CardActions>
               <FlatButton label="CLick here to Contact us" href="/contact"/>
